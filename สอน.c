e397d77e3a8c51febacfd0458a5f697ca1b8fa4a
Chadakhon <cann8680@gmail.com>
2026-09-10 01:31:26 +0700

    อัพโหลด

---
+++ สอน.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float Fahrenheit, Celsius;
+     
+    // รับ input เป็น Fahrenheit
+    printf("inter your body temperature (Fahrenheit):");
+
+    scanf("%f", &Fahrenheit);
+    
+    // แปลง Fahrenheit เป็น Celsius
+    Celsius = (Fahrenheit - 32)*5/9;
+
+    printf("temperature: %.2f, ", Celsius);
+
+    // กำหนด ถ้า Celsius มากกว่า 37.5 จะแสดงว่า บุคคลนี้เป็นไข้ ถ้าไม่ แสดง ไม่มีไข้
+    if (Celsius>=37.5)
+     printf("Result: บุคคลนี้นี้เป็นไข้\n");
+    else 
+      printf("Result: ไม่มีไข้\n");
+      
+    return 0;
+}
